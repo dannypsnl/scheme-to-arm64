783a1042047b1897d3d96aca7d47cd4d87e6e3fb
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-08 00:19:53 +0800

    primitives with operand 1
    * integer?
    * boolean?
    * char?
    * zero?

--- compiler.rkt
+++ compiler.rkt
@@ -46,6 +46,18 @@
 ; Get all arguments of a passed primcall form
 (define (primitive-op-args form) (cddr form))
 
+(define (emit-is-w0-equal-to val)
+  (emit "cmp w0, #~a" val)
+  (emit "b.eq label1")
+  (emit "b label2")
+  (emit "label1:")
+  (emit "mov w0, #~a" (immediate-rep #t))
+  (emit "b end_of_arithmetic_if")
+  (emit "label2:")
+  (emit "mov w0, #~a" (immediate-rep #f))
+  (emit "b end_of_arithmetic_if")
+  (emit "end_of_arithmetic_if:")
+  )
 (define (compile-primitive-call form)
   (case (primitive-op form)
     [(add1)
@@ -53,7 +65,22 @@
      (emit "add w0, w0, #~a" (immediate-rep 1))]
     [(sub1)
      (compile-expr (primitive-op-arg1 form))
-     (emit "sub w0, w0, #~a" (immediate-rep 1))]))
+     (emit "sub w0, w0, #~a" (immediate-rep 1))]
+    [(integer?)
+     (compile-expr (primitive-op-arg1 form))
+     (emit "and w0, w0, #~a" fixnum-mask)
+     (emit-is-w0-equal-to 0)]
+    [(boolean?)
+     (compile-expr (primitive-op-arg1 form))
+     (emit "and w0, w0, #~a" bool-mask)
+     (emit-is-w0-equal-to bool-tag)]
+    [(char?)
+     (compile-expr (primitive-op-arg1 form))
+     (emit "and w0, w0, #~a" char-mask)
+     (emit-is-w0-equal-to char-tag)]
+    [(zero?)
+     (compile-expr (primitive-op-arg1 form))
+     (emit-is-w0-equal-to 0)]))
 (define (compile-expr e)
   (cond
     [(immediate? e)
