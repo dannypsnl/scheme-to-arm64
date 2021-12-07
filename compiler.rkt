0c928391036e9133f3e399203fdad4c0e8f8c5a4
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-08 03:11:53 +0800

    fix compile-expr calls

--- compiler.rkt
+++ compiler.rkt
@@ -72,25 +72,25 @@
      (emit "mov w0, #~a" (immediate-rep #t))
      (label end)]
     [(add1)
-     (compile-expr (primitive-op-arg form 1))
+     (compile-expr (primitive-op-arg form 1) stack-index)
      (emit "add w0, w0, #~a" (immediate-rep 1))]
     [(sub1)
-     (compile-expr (primitive-op-arg form 1))
+     (compile-expr (primitive-op-arg form 1) stack-index)
      (emit "sub w0, w0, #~a" (immediate-rep 1))]
     [(integer?)
-     (compile-expr (primitive-op-arg form 1))
+     (compile-expr (primitive-op-arg form 1) stack-index)
      (emit "and w0, w0, #~a" fixnum-mask)
      (emit-is-w0-equal-to 0)]
     [(boolean?)
-     (compile-expr (primitive-op-arg form 1))
+     (compile-expr (primitive-op-arg form 1) stack-index)
      (emit "and w0, w0, #~a" bool-mask)
      (emit-is-w0-equal-to bool-tag)]
     [(char?)
-     (compile-expr (primitive-op-arg form 1))
+     (compile-expr (primitive-op-arg form 1) stack-index)
      (emit "and w0, w0, #~a" char-mask)
      (emit-is-w0-equal-to char-tag)]
     [(zero?)
-     (compile-expr (primitive-op-arg form 1))
+     (compile-expr (primitive-op-arg form 1) stack-index)
      (emit-is-w0-equal-to 0)]))
 (define (compile-expr e stack-index)
   (cond
