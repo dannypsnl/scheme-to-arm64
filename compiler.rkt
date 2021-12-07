c5111189ebf6f599f361e24feb2c89efb74fe43d
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-08 02:58:55 +0800

    fix nested expression
    close #1

--- compiler.rkt
+++ compiler.rkt
@@ -8,6 +8,7 @@
          "representation.rkt"
          "primitive.rkt")
 
+(define wordsize 4)
 (define (immediate? x) (or (integer? x) (char? x) (boolean? x) (null? x)))
 
 (define (emit-is-w0-equal-to val)
@@ -20,41 +21,28 @@
   (label if-true)
   (emit "mov w0, #~a" (immediate-rep #t))
   (label end))
-(define (compile-primitive-call form)
+(define (compile-primitive-call form stack-index)
   (case (primitive-op form)
-    [(+)
-     (compile-expr (primitive-op-arg form 1))
-     (emit "mov w8, w0")
-     (compile-expr (primitive-op-arg form 2))
-     (emit "mov w9, w0")
-     (emit "add w0, w8, w9")]
-    [(-)
-     (compile-expr (primitive-op-arg form 1))
-     (emit "mov w8, w0")
-     (compile-expr (primitive-op-arg form 2))
-     (emit "mov w9, w0")
-     (emit "sub w0, w8, w9")]
-    [(*)
-     (compile-expr (primitive-op-arg form 1))
-     (emit "mov w8, w0")
-     (compile-expr (primitive-op-arg form 2))
-     (emit "mov w9, w0")
-     (emit "lsr w9, w9, #~a" fixnum-shift)
-     (emit "mul w0, w8, w9")]
-    [(/)
-     (compile-expr (primitive-op-arg form 1))
-     (emit "mov w8, w0")
-     (compile-expr (primitive-op-arg form 2))
-     (emit "mov w9, w0")
-     (emit "lsr w9, w9, #~a" fixnum-shift)
-     (emit "sdiv w0, w8, w9")]
+    [(+ - * /)
+     (compile-expr (primitive-op-arg form 1) stack-index)
+     (emit "str w0, [x29, #~a]" stack-index)
+     (compile-expr (primitive-op-arg form 2) (- stack-index wordsize))
+     (emit "ldr w8, [x29, #~a]" stack-index)
+     (case (primitive-op form)
+       [(+) (emit "add w0, w8, w0")]
+       [(-) (emit "sub w0, w8, w0")]
+       [(*) (emit "lsr w0, w0, #~a" fixnum-shift)
+            (emit "mul w0, w8, w0")]
+       [(/) (emit "lsr w0, w0, #~a" fixnum-shift)
+            (emit "sdiv w0, w8, w0")])]
     [(= < > <= >=)
      (define-label if-true)
      (define-label end)
-     (compile-expr (primitive-op-arg form 1))
-     (emit "mov w8, w0")
-     (compile-expr (primitive-op-arg form 2))
-     (emit "cmp w8, w0")
+     (compile-expr (primitive-op-arg form 1) stack-index)
+     (emit "str w0, [x29, #~a]" stack-index)
+     (compile-expr (primitive-op-arg form 2) (- stack-index wordsize))
+     (emit "ldr w8, [x29, #~a]" stack-index)
+     (emit "cmp w0, w8")
      (emit "mov w0, #~a" (immediate-rep #f))
      (case (primitive-op form)
        [(=) (b.eq if-true)]
@@ -104,11 +92,11 @@
     [(zero?)
      (compile-expr (primitive-op-arg form 1))
      (emit-is-w0-equal-to 0)]))
-(define (compile-expr e)
+(define (compile-expr e stack-index)
   (cond
     [(immediate? e)
      (emit "mov w0, #~a" (immediate-rep e))]
-    [(primitive-call? e) (compile-primitive-call e)]))
+    [(primitive-call? e) (compile-primitive-call e stack-index)]))
 
 (define (compile-program program)
   (emit ".section __TEXT,__text,regular,pure_instructions")
@@ -116,7 +104,9 @@
   (emit ".globl _scheme_entry")
   (emit "_scheme_entry:")
 
-  (compile-expr program)
+  (emit "stp x29, x30, [sp, #-16]!")
+  (emit "mov x29, sp")
+  (compile-expr program (- wordsize))
   (emit "ret"))
 
 (define (compile-to-binary program)
