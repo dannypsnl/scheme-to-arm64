d24f9a119856d8c4b91277aa57c5c3cedcccd62a
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-07 23:26:31 +0800

    encode

--- compiler.rkt
+++ compiler.rkt
@@ -8,8 +8,37 @@
   (apply printf args)
   (newline))
 
+(define fixnum-shift 2)
+(define fixnum-mask 3)
+
+(define ptr-mask 7) ; mask for pointer type tag
+(define ptr-mask-inv #xfffffff8) ; mask for pointer value
+
+(define pair-tag 1)
+(define vec-tag 2)
+(define str-tag 3)
+(define sym-tag 5)
+(define closure-tag 6)
+
+(define char-mask 255) ; character type mask
+(define char-shift 8)
+(define char-tag 7)
+
+(define bool-mask 255)
+(define bool-shift 8)
+(define bool-tag 15)
+
+(define (immediate-rep x)
+  (cond
+    [(integer? x) (bitwise-and (arithmetic-shift x fixnum-shift) #xffffffff)]
+    [(char? x) (bitwise-ior (arithmetic-shift (char->integer x) char-shift) char-tag)]
+    [(boolean? x)
+     (if x
+         (bitwise-ior (arithmetic-shift 1 bool-shift) bool-tag)
+         bool-tag)]))
+
 (define (compile-expr e)
-  (emit "mov w0, #~a" e))
+  (emit "mov w0, #~a" (immediate-rep e)))
 
 (define (compile-program program)
   (emit ".section __TEXT,__text,regular,pure_instructions")
