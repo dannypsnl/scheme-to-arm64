9e880928e6d7a5dd2bb215bb68245755669d570e
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-15 13:25:59 +0800

    cleanup

--- emit.rkt
+++ emit.rkt
@@ -7,19 +7,6 @@
 (define (emit . args)
   (apply printf args)
   (newline))
-(define (b.eq label) (emit "b.eq ~a" label))
-(define (b.ne label) (emit "b.ne ~a" label))
-(define (b.lt label) (emit "b.lt ~a" label))
-(define (b.le label) (emit "b.le ~a" label))
-(define (b.gt label) (emit "b.gt ~a" label))
-(define (b.ge label) (emit "b.ge ~a" label))
-(define (b label) (emit "b ~a" label))
-
-(define-syntax-parser label
-  [(_ name emits ...)
-   #'(begin
-       (emit "~a:" name)
-       emits ...)])
 
 (define-syntax-parser define-label
   [(_ label-names ...)
