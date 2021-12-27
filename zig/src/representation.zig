c888aced1fdeb9fc8735293ee91d073da4a3b4ab
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-27 17:29:11 +0800

    const alias

--- zig/src/representation.zig
+++ zig/src/representation.zig
@@ -3,33 +3,47 @@ const print = @import("std").debug.print;
 const c = @cImport({
     @cInclude("representation.h");
 });
+const FIXNUM_MASK = c.FIXNUM_MASK;
+const FIXNUM_TAG = c.FIXNUM_TAG;
+const FIXNUM_SHIFT = c.FIXNUM_SHIFT;
+const CHAR_MASK = c.CHAR_MASK;
+const CHAR_TAG = c.CHAR_TAG;
+const CHAR_SHIFT = c.CHAR_SHIFT;
+const BOOL_MASK = c.BOOL_MASK;
+const BOOL_TAG = c.BOOL_TAG;
+const BOOL_SHIFT = c.BOOL_SHIFT;
+const PTR_MASK = c.PTR_MASK;
+const PAIR_TAG = c.PAIR_TAG;
+const VOID_TAG = c.VOID_TAG;
+const STR_TAG = c.STR_TAG;
+const VEC_TAG = c.VEC_TAG;
 
 fn putchar(x: u8) void {
     print("{c}", .{x});
 }
 
 pub fn show(x: i64) void {
-    if ((x & c.FIXNUM_MASK) == c.FIXNUM_TAG) {
+    if ((x & FIXNUM_MASK) == FIXNUM_TAG) {
         // integer
-        print("{d}", .{x >> c.FIXNUM_SHIFT});
-    } else if ((x & c.CHAR_MASK) == c.CHAR_TAG) {
+        print("{d}", .{x >> FIXNUM_SHIFT});
+    } else if ((x & CHAR_MASK) == CHAR_TAG) {
         // character
-        const char = @truncate(u8, @intCast(u64, x >> c.CHAR_SHIFT));
+        const char = @truncate(u8, @intCast(u64, x >> CHAR_SHIFT));
         switch (char) {
             0 => print("#\\nul", .{}),
             ' ' => print("#\\space", .{}),
             '\n' => print("#\\newline", .{}),
             else => print("#\\{c}", .{char}),
         }
-    } else if ((x & c.BOOL_MASK) == c.BOOL_TAG) {
+    } else if ((x & BOOL_MASK) == BOOL_TAG) {
         // boolean
-        if ((x >> c.BOOL_SHIFT) != 0) {
+        if ((x >> BOOL_SHIFT) != 0) {
             print("#t", .{});
         } else {
             print("#f", .{});
         }
-    } else if ((x & c.PTR_MASK) == c.VOID_TAG) {} else if ((x & c.PTR_MASK) == c.PAIR_TAG) {
-        var ptr = @intToPtr(?[*]i64, @intCast(usize, x - c.PAIR_TAG));
+    } else if ((x & PTR_MASK) == VOID_TAG) {} else if ((x & PTR_MASK) == PAIR_TAG) {
+        var ptr = @intToPtr(?[*]i64, @intCast(usize, x - PAIR_TAG));
         if (ptr == null) {
             print("()", .{});
             return;
@@ -40,8 +54,8 @@ pub fn show(x: i64) void {
         putchar('(');
         show(car);
         // show additional space-separated elems
-        while ((cdr & c.PTR_MASK) == c.PAIR_TAG) {
-            ptr = @intToPtr(?[*]i64, @intCast(usize, cdr - c.PAIR_TAG));
+        while ((cdr & PTR_MASK) == PAIR_TAG) {
+            ptr = @intToPtr(?[*]i64, @intCast(usize, cdr - PAIR_TAG));
             if (ptr == null) break;
 
             car = ptr.?[0];
@@ -50,13 +64,13 @@ pub fn show(x: i64) void {
             show(car);
         }
         // show dotted pair notation if relevant
-        if ((cdr & c.PTR_MASK) != c.PAIR_TAG) {
+        if ((cdr & PTR_MASK) != PAIR_TAG) {
             print(" . ", .{});
             show(cdr);
         }
         putchar(')');
-    } else if ((x & c.PTR_MASK) == c.STR_TAG) {
-        const ptr = @intToPtr([*]i64, @intCast(usize, x - c.STR_TAG));
+    } else if ((x & PTR_MASK) == STR_TAG) {
+        const ptr = @intToPtr([*]i64, @intCast(usize, x - STR_TAG));
         var len: i64 = ptr[0];
         var body: [*]u8 = @ptrCast([*]u8, ptr + 1);
         putchar('"');
@@ -66,8 +80,8 @@ pub fn show(x: i64) void {
             len -= 1;
         }
         putchar('"');
-    } else if ((x & c.PTR_MASK) == c.VEC_TAG) {
-        var ptr = @intToPtr([*]i64, @intCast(usize, x - c.VEC_TAG));
+    } else if ((x & PTR_MASK) == VEC_TAG) {
+        var ptr = @intToPtr([*]i64, @intCast(usize, x - VEC_TAG));
         var len: i64 = ptr[0];
         ptr += 1;
         print("#(", .{});
