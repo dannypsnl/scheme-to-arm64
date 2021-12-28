9f81c9e8b590685e71f46c9bd632ebf0077cb9a8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-28 10:50:02 +0800

    this is required

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
 const rep = @import("./representation.zig");
 
 extern "c" fn scheme_entry() i64;
@@ -6,5 +7,5 @@ extern "c" fn scheme_entry() i64;
 pub fn main() !void {
     const result: i64 = scheme_entry();
     rep.show(result);
-    std.debug.print("\n", .{});
+    try stdout.print("\n", .{});
 }
