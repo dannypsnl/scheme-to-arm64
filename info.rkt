b33b90e0edaa75a8ce13b859ab2c3bdc8f9475db
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-28 23:14:02 +0800

    rename project

--- info.rkt
+++ info.rkt
@@ -1,8 +1,7 @@
 #lang info
-(define collection "scheme-compiler")
+(define collection "scheme-to-arm64")
 (define deps '("base" "nanopass"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/scheme-compiler.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(林子篆))
